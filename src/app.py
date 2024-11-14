fdf75aa2a5803e0598f2bab86cfd0e35eae446a4
Jose David Caro Cantor <43614556+josecaro02@users.noreply.github.com>
2024-11-14 00:54:26 +0000

    [Add explanation class of project Star Wars API]

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet, Characters
 #from models import Person
 
 app = Flask(__name__)
@@ -38,12 +38,76 @@ def sitemap():
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
-
     response_body = {
         "msg": "Hello, this is your GET /user response "
     }
-
     return jsonify(response_body), 200
+
+
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    users = User.query.all()
+    print(users)# users es una lista con todos los usuarios
+    users_serialized = []
+    for user in users:
+        users_serialized.append(user.serialize())
+    #serializar es convertir un dato tipo modelo a dict
+    #solo asi se puede convertir en JSON
+    return jsonify({'msg': 'ok', 'data': users_serialized}), 200
+
+@app.route('/planet', methods=['POST'])
+def post_planet():
+    #para crear un planeta necesitamos un body que contenga
+    #el nombre y el clima del planeta
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({'msg': 'Debes enviar información en el body'}), 400
+    if 'name' not in body:
+        return jsonify({'msg': 'El campo name es obligatorio'}), 400
+    if 'climate' not in body:
+        return jsonify({'msg': 'El campo climate es obligatorio'}), 400
+    new_planet = Planet()
+    new_planet.name = body['name']
+    new_planet.climate = body['climate']
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return jsonify(
+        {
+            'msg': 'Planeta agregado con exito',
+          'data': new_planet.serialize()
+          }
+          ), 201
+
+
+@app.route('/characters/<int:id>', methods=['GET'])
+def get_characters(id):
+    character = Characters.query.get(id)
+    print(character) #objeto
+    print(character.name)
+    print(character.planet_id)
+    print(character.planet_id_relationship) #objeto
+    print(character.planet_id_relationship.name)
+    character_serialized = character.serialize()
+    #character_serialized['planet_info'] = character.planet_id_relationship.serialize()
+    return jsonify({
+        'msg': 'ok',
+        'data': character_serialized
+        })
+
+@app.route('/favorite_planets/<int:user_id>', methods=['GET'])
+def get_favorites_by_user(user_id):
+    user = User.query.get(user_id)
+    #print(user)
+    #print(user.planet_favorites)
+    favorite_planets_serialized = []
+    for fav_planet in user.planet_favorites:
+        favorite_planets_serialized.append(fav_planet.planet_relationship.serialize())
+    data = {
+        'user_info': user.serialize(),
+        'planets_favorites': favorite_planets_serialized
+    }
+    return jsonify({'msg':'ok', 'data': data})
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -3,17 +3,76 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    planet_favorites = db.relationship('Planet_Favorites', back_populates='user_relationship')
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'Usuario {self.email} y id {self.id}'
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "is_active": self.is_active,
+            "password": self.password
             # do not serialize the password, its a security breach
+        }
+
+class Characters(db.Model):
+    __tablename__ = "characters"
+    id = db.Column(db.Integer, primary_key= True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    height = db.Column(db.Integer, nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=False)
+    planet_id_relationship = db.relationship('Planet', back_populates='people')
+
+    def __repr__(self):
+        return f'Character {self.name} con ID {self.id}'
+    
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'height': self.height,
+            'planet_info': self.planet_id_relationship.serialize()
+        }
+
+class Planet(db.Model):
+    __tablename__ = "planet"
+    id = db.Column(db.Integer, primary_key= True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    climate = db.Column(db.String(50), nullable=False)
+    people = db.relationship('Characters', back_populates='planet_id_relationship')
+    favorite_planets = db.relationship('Planet_Favorites', back_populates='planet_relationship')
+
+    def __repr__(self):
+        return f'Planeta {self.name} con clima {self.climate}'
+    
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'climate': self.climate
+        }
+
+class Planet_Favorites(db.Model):
+    __tablename__ = 'planet_favorites'
+    id = db.Column(db.Integer, primary_key= True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_relationship = db.relationship('User', back_populates='planet_favorites')
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    planet_relationship = db.relationship('Planet', back_populates='favorite_planets')
+
+    def __repr__(self):
+        return f'Al usuario {self.user_id}, le gusta el planeta {self.planet_id}'
+    
+    def serialize(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'planet_id': self.planet_id
         }
